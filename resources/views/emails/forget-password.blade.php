b0ed4f272d042d7ab7d8222bfb3637bcb016f209
francesko004 <franc3sko@proton.me>
2025-05-19 09:04:28 +0200

    Update forget-password.blade.php

--- resources/views/emails/forget-password.blade.php
+++ resources/views/emails/forget-password.blade.php
@@ -10,19 +10,19 @@
             <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="max-width: 600px; margin: 0 auto;">
                 <tr>
                     <td style="background-color: #fff; padding: 20px; border-radius: 10px; box-shadow: 0 1px 4px rgba(0, 0, 0, 0.16);">
-                        <h2 style="margin-bottom: 20px;">Redefinição de Senha</h2>
+                        <h2 style="margin-bottom: 20px;">Password Reset</h2>
                         <p>
-                            Você está recebendo este e-mail porque recebemos uma solicitação para redefinir a senha da sua conta. O token é esse abaixo:
+                            You are receiving this email because we received a request to reset your account password. The token is shown below:
                             <br>
                             <br>
                             <br>
                             <code style="padding: 5px; background-color: #9ca3af46;">{{ $token }}</code>
                             <br>
                             <br>
-                            <a href="{{ $resetLink }}" style="background-color: #007BFF; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Redefinir Senha</a>
+                            <a href="{{ $resetLink }}" style="background-color: #007BFF; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Reset Password</a>
                         </p>
                         <p>
-                            Se você não solicitou uma redefinição de senha, ignore este e-mail.
+                            If you did not request a password reset, please ignore this email.
                         </p>
                     </td>
                 </tr>
