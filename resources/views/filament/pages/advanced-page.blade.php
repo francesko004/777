6ffcd6b162e3295e5802a7f6ebf7c8f83b5c3073
francesko004 <franc3sko@proton.me>
2025-05-19 08:51:04 +0200

    Update advanced-page.blade.php

--- resources/views/filament/pages/advanced-page.blade.php
+++ resources/views/filament/pages/advanced-page.blade.php
@@ -5,27 +5,27 @@
         <div class="flex justify-between w-full gap-4">
             <div class="flex flex-col gap-4 w-full justify-between">
                 <button wire:click="loadProvider('slot')" class="bg-primary-600 px-3 py-2 w-full text-center">
-                    <div wire:loading wire:target="loadProvider('slot')">Carregando Provedores</div>
-                    <div wire:loading.remove wire:target="loadProvider('slot')">Carregar Provedor (Slot)</div>
+                    <div wire:loading wire:target="loadProvider('slot')">Loading Providers</div>
+                    <div wire:loading.remove wire:target="loadProvider('slot')">Load Provider (Slot)</div>
                 </button>
                 <button wire:click="loadProvider('casino')" class="bg-primary-600 px-3 py-2 w-full">
-                    <div wire:loading wire:target="loadProvider('casino')">Carregando Provedores</div>
-                    <div wire:loading.remove wire:target="loadProvider('casino')">Carregar Provedor (Casino)</div>
+                    <div wire:loading wire:target="loadProvider('casino')">Loading Providers</div>
+                    <div wire:loading.remove wire:target="loadProvider('casino')">Load Provider (Casino)</div>
                 </button>
                 <button wire:click="loadProvider('pachinko')" class="bg-primary-600 px-3 py-2 w-full">
-                    <div wire:loading wire:target="loadProvider('pachinko')">Carregando Provedores</div>
-                    <div wire:loading.remove wire:target="loadProvider('pachinko')">Carregar Provedor (Pachinko)</div>
+                    <div wire:loading wire:target="loadProvider('pachinko')">Loading Providers</div>
+                    <div wire:loading.remove wire:target="loadProvider('pachinko')">Load Provider (Pachinko)</div>
                 </button>
             </div>
             <button wire:click="loadGames" class="bg-primary-500 px-3 py-2 w-full">
-                <div wire:loading wire:target="loadGames">Carregando Jogos</div>
-                <div wire:loading.remove wire:target="loadGames">Carregar Jogos</div>
+                <div wire:loading wire:target="loadGames">Loading Games</div>
+                <div wire:loading.remove wire:target="loadGames">Load Games</div>
             </button>
         </div>
     </div>
 
     <div class="w-full p-4 bg-gray-500 shadow-lg" style="background-color: #51515163;border-radius: 5px;">
-        <h2 class="mb-5 text-3xl">Atualizações</h2>
+        <h2 class="mb-5 text-3xl">Updates</h2>
         <hr style="border-color: #4b4b4b8c;padding-bottom: 10px">
 
         @if($output)
@@ -38,15 +38,15 @@
 
         <div class="flex justify-between w-full gap-4 mb-3">
             <button wire:click="runMigrate" class="bg-primary-600 px-3 py-2 w-full text-center">
-                <div wire:loading wire:target="runMigrate">Rodando as migrações</div>
-                <div wire:loading.remove wire:target="runMigrate">Rodar as Migrações</div>
+                <div wire:loading wire:target="runMigrate">Running migrations</div>
+                <div wire:loading.remove wire:target="runMigrate">Run Migrations</div>
             </button>
         </div>
 
         <div class="flex justify-between w-full gap-4 mb-3">
             <button wire:click="runMigrateWithSeed" class="bg-primary-600 px-3 py-2 w-full text-center">
-                <div wire:loading wire:target="runMigrateWithSeed">Rodando as migrações com seed</div>
-                <div wire:loading.remove wire:target="runMigrateWithSeed">Rodar as Migrações com Seed</div>
+                <div wire:loading wire:target="runMigrateWithSeed">Running migrations with seed</div>
+                <div wire:loading.remove wire:target="runMigrateWithSeed">Run Migrations with Seed</div>
             </button>
         </div>
 
@@ -58,11 +58,8 @@
 
             <br>
             <x-filament::button type="submit" form="submit" class="w-full">
-                Carregar Arquivo
+                Load File
             </x-filament::button>
         </form>
     </div>
-
-
-
 </x-filament-panels::page>
