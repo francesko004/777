ecb0c6d3135354880fd206f30d2a7dad6b430896
vortex378 <arkellahi81@gmail.com>
2025-05-19 08:21:26 +0200

    Update DashboardAdmin.php

--- app/Filament/Pages/DashboardAdmin.php
+++ app/Filament/Pages/DashboardAdmin.php
@@ -2,13 +2,9 @@
 
 namespace App\Filament\Pages;
 
-
 use App\Filament\Widgets\StatsOverview;
-
-
 use App\Livewire\WalletOverview;
 use Illuminate\Support\HtmlString;
-
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Form;
@@ -25,9 +21,8 @@ class DashboardAdmin extends \Filament\Pages\Dashboard
      */
     public function getSubheading(): string| null|\Illuminate\Contracts\Support\Htmlable
     {
-        return "Bem-vindo(a) de volta, Admin! Seu painel está pronto para você.";
+        return "Welcome back, Admin! Your dashboard is ready for you.";
     }
-    
 
     /**
      * @param Form $form
@@ -37,44 +32,44 @@ class DashboardAdmin extends \Filament\Pages\Dashboard
     {
         return $form
             ->schema([
-                Section::make('ONDA GAMES CRIOU ESSA PLATAFORMA PARA VOCÊ')
-                ->description(new HtmlString('
-                    <div style="font-weight: 600; display: flex; align-items: center;">
-                        SAIBA MAIS SOBRE NÓS. PARTICIPE DA NOSSA COMUNIDADE IGAMING. ACESSE AGORA! 
-                        <a class="dark:text-white" 
-                           style="
-                                font-size: 14px;
-                                font-weight: 600;
-                                width: 127px;
-                                display: flex;
-                                background-color: #f800ff;
-                                padding: 10px;
-                                border-radius: 11px;
-                                justify-content: center;
-                                margin-left: 10px;
-                           " 
-                           href="https://ondagames.com" 
-                           target="_blank">
-                            SITE OFICIAL
-                        </a>
-                        <a class="dark:text-white" 
-                           style="
-                                font-size: 14px;
-                                font-weight: 600;
-                                width: 127px;
-                                display: flex;
-                                background-color: #f800ff;
-                                padding: 10px;
-                                border-radius: 11px;
-                                justify-content: center;
-                                margin-left: 10px;
-                           " 
-                           href="https://t.me/ondagames_oficial" 
-                           target="_blank">
-                            GRUPO TELEGRAM
-                        </a>
-                    </div>
-            ')),
+                Section::make('ONDA GAMES CREATED THIS PLATFORM FOR YOU')
+                    ->description(new HtmlString('
+                        <div style="font-weight: 600; display: flex; align-items: center;">
+                            LEARN MORE ABOUT US. JOIN OUR IGAMING COMMUNITY. ACCESS NOW! 
+                            <a class="dark:text-white" 
+                               style="
+                                    font-size: 14px;
+                                    font-weight: 600;
+                                    width: 127px;
+                                    display: flex;
+                                    background-color: #f800ff;
+                                    padding: 10px;
+                                    border-radius: 11px;
+                                    justify-content: center;
+                                    margin-left: 10px;
+                               " 
+                               href="https://ondagames.com " 
+                               target="_blank">
+                                OFFICIAL SITE
+                            </a>
+                            <a class="dark:text-white" 
+                               style="
+                                    font-size: 14px;
+                                    font-weight: 600;
+                                    width: 127px;
+                                    display: flex;
+                                    background-color: #f800ff;
+                                    padding: 10px;
+                                    border-radius: 11px;
+                                    justify-content: center;
+                                    margin-left: 10px;
+                               " 
+                               href="https://t.me/ondagames_oficial " 
+                               target="_blank">
+                                TELEGRAM GROUP
+                            </a>
+                        </div>
+                    ')),
             ]);
     }
 
@@ -85,14 +80,13 @@ class DashboardAdmin extends \Filament\Pages\Dashboard
     {
         return [
             FilterAction::make()
-                ->label('Filtro')
+                ->label('Filter')
                 ->form([
-                    DatePicker::make('startDate')->label('Data Incial'),
-                    DatePicker::make('endDate')->label('Data Final'),
+                    DatePicker::make('startDate')->label('Start Date'),
+                    DatePicker::make('endDate')->label('End Date'),
                 ]),
         ];
     }
-
 
     /**
      * @return string[]
@@ -100,10 +94,6 @@ class DashboardAdmin extends \Filament\Pages\Dashboard
     public function getWidgets(): array
     {
         return [
-
-
-
-
             WalletOverview::class,
             StatsOverview::class,
         ];
