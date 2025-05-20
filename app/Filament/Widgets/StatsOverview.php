f534c5d62a21f96abd7a4c031884137080466336
vortex378 <arkellahi81@gmail.com>
2025-05-20 09:21:16 +0200

    Update StatsOverview.php

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -22,13 +22,13 @@ class StatsOverview extends BaseWidget
     {
         $today = Carbon::today();
 
-        // Cálculos dos saldos de todos os jogadores
+        // Calculations of balances for all players
         $saldodosplayers = DB::table('wallets')
             ->join('users', 'users.id', '=', 'wallets.user_id')
             ->sum(DB::raw('wallets.balance + wallets.balance_bonus + wallets.balance_withdrawal'));
     
 
-        // Total depositado e sacado hoje
+        // Total deposited and withdrawn today
         $totalDepositedToday = DB::table('deposits')
             ->whereDate('created_at', $today)
             ->where('status', '1')
@@ -38,11 +38,11 @@ class StatsOverview extends BaseWidget
             ->where('status', '1')
             ->sum('amount');
 
-        // Ganhos de afiliados a pagar
+        // Affiliate earnings to be paid
         $totalReferRewardsLast7Days = Wallet::where('refer_rewards', '>=', 1)
             ->sum('refer_rewards');
 
-        // Cálculo dos depósitos
+        // Deposit calculations
         $depositCounts = DB::table('deposits')
             ->select('user_id', DB::raw('count(*) as deposit_count'))
             ->where('status', '1')
@@ -55,64 +55,64 @@ class StatsOverview extends BaseWidget
         $numberOfUsersWithFourOrMoreDeposits = $depositCounts->where('deposit_count', '>=', 4)->count();
 
         return [
-            Stat::make('TOTAL DE CADASTROS', User::count())
-                ->description('Total de usuários cadastrados')
+            Stat::make('TOTAL REGISTRATIONS', User::count())
+                ->description('Total registered users')
                 ->descriptionIcon('heroicon-o-user')
                 ->color('blue')
                 ->chart([10, 20, 15, 30, 25, 40, 35])
                 ->chartColor('rgba(59, 130, 246, 0.5)'),
             
-            Stat::make('TOTAL DEPOSITADO HOJE', \Helper::amountFormatDecimal($totalDepositedToday)) 
-                ->description('Total depositado hoje')
+            Stat::make('TOTAL DEPOSITED TODAY', \Helper::amountFormatDecimal($totalDepositedToday)) 
+                ->description('Total deposited today')
                 ->descriptionIcon('heroicon-o-currency-dollar')
                 ->color('green')
                 ->chart([30, 50, 40, 60, 80, 70, 90])
                 ->chartColor('rgba(16, 185, 129, 0.5)'),
             
-            Stat::make('TOTAL DE SAQUES HOJE', \Helper::amountFormatDecimal($totalsacadoToday))
-                ->description('Total sacado hoje')
+            Stat::make('TOTAL WITHDRAWN TODAY', \Helper::amountFormatDecimal($totalsacadoToday))
+                ->description('Total withdrawn today')
                 ->descriptionIcon('heroicon-o-arrow-down')
                 ->color('red')
                 ->chart([40, 30, 50, 60, 70, 90, 100])
                 ->chartColor('rgba(239, 68, 68, 0.5)'),
 
-            Stat::make('SALDO TOTAL DOS PLAYERS', \Helper::amountFormatDecimal($saldodosplayers))
-                ->description('Saldo nas contas dos jogadores')
+            Stat::make('TOTAL PLAYER BALANCE', \Helper::amountFormatDecimal($saldodosplayers))
+                ->description('Balance in player accounts')
                 ->descriptionIcon('heroicon-o-credit-card')
                 ->color('purple')
                 ->chart([15, 30, 25, 40, 35, 50, 45])
                 ->chartColor('rgba(139, 92, 246, 0.5)'),
 
-            Stat::make('SALDO TOTAL DOS AFILIADOS', \Helper::amountFormatDecimal($totalReferRewardsLast7Days))
-                ->description('Ganho acumulado dos afiliados')
+            Stat::make('TOTAL AFFILIATE BALANCE', \Helper::amountFormatDecimal($totalReferRewardsLast7Days))
+                ->description('Accumulated affiliate earnings')
                 ->descriptionIcon('heroicon-o-briefcase')
                 ->color('yellow')
                 ->chart([5, 15, 10, 20, 25, 30, 35])
                 ->chartColor('rgba(250, 204, 21, 0.5)'),
             
-            Stat::make('PESSOAS QUE DEPOSITARAM 1 VEZ', $numberOfUsersWithSingleDeposit) // 
-                ->description('Depósitos únicos')
+            Stat::make('USERS WITH 1 DEPOSIT', $numberOfUsersWithSingleDeposit) // 
+                ->description('Single deposits')
                 ->descriptionIcon('heroicon-o-user-group')
                 ->color('orange')
                 ->chart([10, 20, 15, 30, 25, 35, 45])
                 ->chartColor('rgba(249, 115, 22, 0.5)'),
             
-            Stat::make('PESSOAS QUE DEPOSITARAM 2 VEZES', $numberOfUsersWithTwoDeposits)
-                ->description('Depósitos duplos')
+            Stat::make('USERS WITH 2 DEPOSITS', $numberOfUsersWithTwoDeposits)
+                ->description('Double deposits')
                 ->descriptionIcon('heroicon-o-user-group')
                 ->color('pink')
                 ->chart([20, 30, 25, 35, 45, 50, 55])
                 ->chartColor('rgba(236, 72, 153, 0.5)'),
             
-            Stat::make('PESSOAS QUE DEPOSITARAM 3 VEZES', $numberOfUsersWithThreeDeposits)
-                ->description('Depósitos triplos')
+            Stat::make('USERS WITH 3 DEPOSITS', $numberOfUsersWithThreeDeposits)
+                ->description('Triple deposits')
                 ->descriptionIcon('heroicon-o-user-group')
                 ->color('indigo')
                 ->chart([15, 25, 20, 30, 35, 40, 50])
                 ->chartColor('rgba(79, 70, 229, 0.5)'),
 
-            Stat::make('PESSOAS QUE DEPOSITARAM 4 VEZES', $numberOfUsersWithFourOrMoreDeposits)
-                ->description('Depósitos frequentes')
+            Stat::make('USERS WITH 4+ DEPOSITS', $numberOfUsersWithFourOrMoreDeposits)
+                ->description('Frequent deposits')
                 ->descriptionIcon('heroicon-o-user-group')
                 ->color('teal')
                 ->chart([25, 35, 30, 40, 45, 55, 60])
